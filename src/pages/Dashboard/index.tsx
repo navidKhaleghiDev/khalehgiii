81d7e32cbb25593635af6e3c25868210307aab63
m.jafarabadi <85762407+mahdijf7@users.noreply.github.com>
2023-12-25 17:20:19 +0330

    feat:( i18 local)

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,31 +1,31 @@
-import { SettingsKeycloak } from "./SettingsKeycloak";
-import { DashboardCards } from "./DashboardCards";
-import { ContainerDashboard } from "@ui/Templates/ContainerDashboard";
-import { useUserContext } from "@context/user/userContext";
-import { Daas } from "./Daas";
-import { BaseTab, BaseTabs } from "@ui/atoms/BaseTabs";
-import { DlpConfig } from "./DlpConfig";
-import { DaasConfig } from "./DaasConfig";
+import { SettingsKeycloak } from './SettingsKeycloak';
+import { DashboardCards } from './DashboardCards';
+import { ContainerDashboard } from '@ui/Templates/ContainerDashboard';
+import { useUserContext } from '@context/user/userContext';
+import { Daas } from './Daas';
+import { BaseTab, BaseTabs } from '@ui/atoms/BaseTabs';
+import { DlpConfig } from './DlpConfig';
+import { DaasConfig } from './DaasConfig';
 
 export function DashboardPage() {
-  const { user } = useUserContext();
+	const { user } = useUserContext();
 
-  return !user?.is_superuser ? (
-    <Daas src={`http://${user?.base_url}:${user?.http_port}`} />
-  ) : (
-    <ContainerDashboard>
-      <DashboardCards />
-      <BaseTabs label="تنظیمات">
-        <BaseTab label="Keycloak">
-          <SettingsKeycloak user={user} />
-        </BaseTab>
-        <BaseTab label="Daas">
-          <DaasConfig />
-        </BaseTab>
-        <BaseTab label="DLP">
-          <DlpConfig />
-        </BaseTab>
-      </BaseTabs>
-    </ContainerDashboard>
-  );
+	return !user?.is_superuser ? (
+		<Daas src={`http://${user?.base_url}:${user?.http_port}`} />
+	) : (
+		<ContainerDashboard>
+			<DashboardCards />
+			<BaseTabs label="تنظیمات">
+				<BaseTab label="Keycloak">
+					<SettingsKeycloak user={user} />
+				</BaseTab>
+				<BaseTab label="Daas">
+					<DaasConfig />
+				</BaseTab>
+				<BaseTab label="DLP">
+					<DlpConfig />
+				</BaseTab>
+			</BaseTabs>
+		</ContainerDashboard>
+	);
 }
