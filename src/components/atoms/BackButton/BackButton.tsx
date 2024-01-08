868766e343f5dbb89295a001521522224f2248a7
ahmad safari <sargashte118@gmail.com>
2024-01-08 20:30:16 +0330

    test husky config

--- src/components/atoms/BackButton/BackButton.tsx
+++ src/components/atoms/BackButton/BackButton.tsx
@@ -1,49 +1,49 @@
 import { useNavigate } from 'react-router-dom';
 import arrowLineLeft from '@iconify-icons/ph/arrow-line-left';
 
-import { BaseButton, IconButton } from '../BaseButton';
 import { useTranslation } from 'react-i18next';
 import { useLanguage } from '@context/settings/languageContext';
+import { BaseButton, IconButton } from '../BaseButton';
 
 export type BackButtonProps = {
-	withLabel?: boolean;
-	onClick?: () => void;
-	className?: string;
+  withLabel?: boolean;
+  onClick?: () => void;
+  className?: string;
 };
 
 export function BackButton({ withLabel, onClick, className }: BackButtonProps) {
-	const navigate = useNavigate();
-	const { t } = useTranslation();
-	const { lang } = useLanguage();
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+  const { lang } = useLanguage();
 
-	const config = {
-		endIcon: lang === 'fa' ? arrowLineLeft : 'null',
-		startIcon: lang === 'en' ? arrowLineLeft : 'null',
-	};
+  const config = {
+    endIcon: lang === 'fa' ? arrowLineLeft : 'null',
+    startIcon: lang === 'en' ? arrowLineLeft : 'null',
+  };
 
-	const handleClick = () => {
-		if (!onClick) {
-			navigate(-1);
-		} else {
-			onClick();
-		}
-	};
+  const handleClick = () => {
+    if (!onClick) {
+      navigate(-1);
+    } else {
+      onClick();
+    }
+  };
 
-	return !withLabel ? (
-		<IconButton
-			onClick={handleClick}
-			icon={arrowLineLeft}
-			size="xl"
-			type="button"
-			color="teal"
-			className={className}
-		/>
-	) : (
-		<BaseButton
-			label={t('global.pageBack')}
-			onClick={handleClick}
-			className={className}
-			{...config}
-		/>
-	);
+  return !withLabel ? (
+    <IconButton
+      onClick={handleClick}
+      icon={arrowLineLeft}
+      size="xl"
+      type="button"
+      color="teal"
+      className={className}
+    />
+  ) : (
+    <BaseButton
+      label={t('global.pageBack')}
+      onClick={handleClick}
+      className={className}
+      {...config}
+    />
+  );
 }
