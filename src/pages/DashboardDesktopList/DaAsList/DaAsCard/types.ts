95b7202f942b011417c5b684c07af1d2f85e3f56
m.jafarabadi <m.jafarabadi@npd-co.ir>
2024-01-22 02:45:04 -0500

    add (removed type )

---
+++ src/pages/DashboardDesktopList/DaAsList/DaAsCard/types.ts
@@ -0,0 +1,12 @@
+import { IDaAs } from '@src/services/users/types';
+
+export type ActionOnClickActionsType =
+  | 'delete'
+  | 'edit'
+  | 'details'
+  | 'mutate'
+  | 'editLock';
+export type OnClickActionsType = (
+  action: ActionOnClickActionsType,
+  daas?: Partial<IDaAs> | string
+) => void;
