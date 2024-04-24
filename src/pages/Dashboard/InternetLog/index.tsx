5782a52685385a49ec9c4da8b5c8754cf462db82
m.jafarabadi <m.jafarabadi@npd-co.ir>
2024-04-24 04:51:25 -0400

    feat (internet log )

--- src/pages/Dashboard/InternetLog/index.tsx
+++ src/pages/Dashboard/InternetLog/index.tsx
@@ -1,8 +1,8 @@
-/* eslint-disable jsx-a11y/iframe-has-title */
 export function InternetLog() {
   return (
     <div className="flex w-full items-center justify-center  mt-40">
       <iframe
+        title="internet-log"
         className="w-full h-screen scale-125"
         src={import.meta.env.VITE_SARG_BASE_URL}
       />
