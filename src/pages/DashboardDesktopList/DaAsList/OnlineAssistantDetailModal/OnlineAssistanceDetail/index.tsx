d979e1e171a91c36071522cd7068d4c3cd56c24c
mahdi jafarabadi <m.jafarabadi@npd-co.ir>
2024-08-05 06:06:31 -0400

    style : shadow

--- src/pages/DashboardDesktopList/DaAsList/OnlineAssistantDetailModal/OnlineAssistanceDetail/index.tsx
+++ src/pages/DashboardDesktopList/DaAsList/OnlineAssistantDetailModal/OnlineAssistanceDetail/index.tsx
@@ -15,21 +15,22 @@ export default function OnlineAssistanceDetail({
     <div>
       <Card
         color="tealDark"
-        className="h-10 flex justify-center items-center  font-semibold	p-2 rounded-xl	 "
+        className="h-10 flex justify-center items-center  font-semibold	p-2 rounded-xl"
+        shadow="sm"
       >
         <Typography variant="body3" color="white" className="section-title ">
           {title}
         </Typography>
       </Card>
-      <div className=" my-2 ">
+      <div className="  ">
         {Array.isArray(data) && data.length >= 1 ? (
           data.map((item) => (
             <Card
               color="white"
               border
               borderColor="neutral"
-              shadow="md"
-              className="h-16 flex justify-start items-center	p-4 border-2 rounded-xl"
+              shadow="sm"
+              className="h-16 flex justify-start items-center	p-4 border-2 rounded-xl my-2"
               key={item}
             >
               <Typography variant="body2"> {Object.keys(item)}</Typography>
