a5a0c1c0286a6234b80fbae441476b4a012b8cb3
ahmad safari <sargashte118@gmail.com>
2024-08-07 09:02:22 -0400

    refactor: data mock

--- src/pages/Dashboard/OnlineAssistance/OnlineAssistanceList/dataMock.ts
+++ src/pages/Dashboard/OnlineAssistance/OnlineAssistanceList/dataMock.ts
@@ -10,7 +10,13 @@ export const onlineAssistanceListDataMock: OnlineAssistanceModel[] = Array.from(
     last_uptime: '2024-06-01T08:53:09.354736',
     duration_time: 24.0,
     knowledge_management_record_path: '/home/mohammadjavad/Downloads/out4.mkv',
-    admin: '0a3d8086-3eb7-4422-9625-bd0a6b29aca3',
-    user: '1bd39ca7-c4e5-4311-aee5-d80d2722daba',
+    admin: {
+      id: '1bd39ca7-c4e5-4311-aee5-d80d2722daba',
+      email: 'admin@admin.com',
+    },
+    user: {
+      id: '1bd39ca7-c4e5-4311-aee5-d80d2722daba',
+      email: 'user@user.com',
+    },
   })
 );
