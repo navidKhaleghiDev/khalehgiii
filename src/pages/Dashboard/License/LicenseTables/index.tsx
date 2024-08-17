ade33c2e07552938a03628660b3e757788f11c86
mahdi jafarabadi <m.jafarabadi@npd-co.ir>
2024-08-17 06:46:57 -0400

    feat license new table & remove malware

--- src/pages/Dashboard/License/LicenseTables/index.tsx
+++ src/pages/Dashboard/License/LicenseTables/index.tsx
@@ -1,35 +1,18 @@
-import { E_USERS_LICENSES } from '@src/services/users/endpoint';
-import { BaseTable } from '@ui/atoms/BaseTable';
-import useSWR from 'swr';
-import { http, HTTP_ANALYSES } from '@src/services/http';
-import { useUserPermission } from '@src/helper/hooks/usePermission';
-import { checkPermissionHeaderItem } from '@ui/atoms/BaseTable/components/utils/CheckPermissionHeaderItem';
-import { E_ANALYZE_SCANNER } from '@src/services/analyze/endpoint';
-import { licenseHeaderItem } from './licenseTableHeaderItem';
+import { NoResult } from '@ui/molecules/NoResult';
+import { LicenseTableBody } from './LicenseTableBody';
+import { LicenseTableHeader } from './LicenseTableHeader';
 
-export function LicenseTables() {
-  const userPermissions = useUserPermission();
-
-  const { data: listData, isLoading } = useSWR(
-    E_ANALYZE_SCANNER,
-    HTTP_ANALYSES.fetcherSWR
-  );
-  const listWhiteList = listData?.data ?? {};
-
-  const { data, isLoading: loading } = useSWR(
-    E_USERS_LICENSES,
-    http.fetcherSWR
-  );
-
-  const listDaas = data?.data ?? [];
-
+export function LicenseTables({ fileType }) {
   return (
-    <div className="p-5">
-      <BaseTable
-        loading={loading || isLoading}
-        headers={checkPermissionHeaderItem(userPermissions, licenseHeaderItem)}
-        bodyList={[{ ...listDaas, ...listWhiteList, id: listWhiteList?.id }]}
-      />
+    <div>
+      <LicenseTableHeader />
+      {fileType.length >= 1 ? (
+        fileType.map((item) => (
+          <LicenseTableBody key={item.number} item={item} />
+        ))
+      ) : (
+        <NoResult />
+      )}
     </div>
   );
 }
--- src/pages/Dashboard/License/index.tsx
+++ src/pages/Dashboard/License/index.tsx
@@ -1,9 +1,20 @@
+import { useUserPermission } from '@src/helper/hooks/usePermission';
+import { http } from '@src/services/http';
+import useSWR from 'swr';
+import { E_USERS_LICENSES } from '@src/services/users/endpoint';
+import { LoadingSpinner } from '@ui/molecules/Loading';
 import { LicenseTables } from './LicenseTables';
 
 export function LicenseCp() {
+  const userPermissions = useUserPermission();
+
+  const { data, isLoading } = useSWR(E_USERS_LICENSES, http.fetcherSWR);
+
+  const listDaas = data?.data ?? [];
+
   return (
-    <div>
-      <LicenseTables />
+    <div className="p-5">
+      {!isLoading ? <LicenseTables fileType={listDaas} /> : <LoadingSpinner />}
     </div>
   );
 }
