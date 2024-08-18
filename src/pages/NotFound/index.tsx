d71eb5f7229d7deeb1af14af98a6574e90990782
mahdi jafarabadi <m.jafarabadi@npd-co.ir>
2024-08-18 07:12:39 -0400

    remove : notfound remove auth

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,38 +1,11 @@
-import { useCallback, useEffect } from 'react';
 import { ROUTES_PATH } from '@src/routes/routesConstants';
 import { BaseButton } from '@ui/atoms/BaseButton';
 import { NoResult } from '@ui/molecules/NoResult';
-import { Link, useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { useUserContext } from '@context/user/userContext';
-import { http, STORAGE_KEY_REFRESH_TOKEN } from '@src/services/http';
-import { API_USERS_LOGOUT } from '@src/services/users';
+import { Link } from 'react-router-dom';
 
 function NotFoundPage() {
-  const navigate = useNavigate();
   const { t } = useTranslation();
-  const { user, setUser } = useUserContext();
-  const refresh = localStorage.getItem(STORAGE_KEY_REFRESH_TOKEN);
-
-  const logoutFunction = useCallback(async () => {
-    const data = {
-      refresh_token: refresh || '',
-    };
-    await API_USERS_LOGOUT(data);
-  }, [refresh]);
-
-  const logout = useCallback(() => {
-    logoutFunction();
-    setUser(null);
-    http.removeAuthHeader();
-    navigate(ROUTES_PATH.login);
-  }, [logoutFunction, navigate, setUser]);
-
-  useEffect(() => {
-    if (user === null) {
-      logout();
-    }
-  }, [user, navigate, logout]);
 
   return (
     <div className="p-16 h-screen flex flex-col justify-center items-center font-on">
