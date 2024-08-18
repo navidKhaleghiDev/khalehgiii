f501754557d4f0c32420b4976e64d4a4fd225603
mahdi jafarabadi <m.jafarabadi@npd-co.ir>
2024-08-18 06:41:13 -0400

    fix : notFound page logout

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,22 +1,39 @@
+import { useCallback, useEffect } from 'react';
 import { ROUTES_PATH } from '@src/routes/routesConstants';
 import { BaseButton } from '@ui/atoms/BaseButton';
 import { NoResult } from '@ui/molecules/NoResult';
-// import { Link, useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-// import { useEffect } from 'react';
-// import { useUserContext } from '@context/user/userContext';
+import { useUserContext } from '@context/user/userContext';
+import { http, STORAGE_KEY_REFRESH_TOKEN } from '@src/services/http';
+import { API_USERS_LOGOUT } from '@src/services/users';
 
 function NotFoundPage() {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const { t } = useTranslation();
-  // const { user } = useUserContext();
+  const { user, setUser } = useUserContext();
+  const refresh = localStorage.getItem(STORAGE_KEY_REFRESH_TOKEN);
 
-  // useEffect(() => {
-  //   if (user === null) {
-  //     navigate(ROUTES_PATH.login);
-  //   } else navigate(ROUTES_PATH.dashboard);
-  // }, [user, navigate]);
+  const logoutFunction = useCallback(async () => {
+    const data = {
+      refresh_token: refresh || '',
+    };
+    await API_USERS_LOGOUT(data);
+  }, [refresh]);
+
+  const logout = useCallback(() => {
+    logoutFunction();
+    setUser(null);
+    http.removeAuthHeader();
+    navigate(ROUTES_PATH.login);
+  }, [logoutFunction, navigate, setUser]);
+
+  useEffect(() => {
+    if (user === null) {
+      logout();
+    }
+  }, [user, navigate, logout]);
 
   return (
     <div className="p-16 h-screen flex flex-col justify-center items-center font-on">
