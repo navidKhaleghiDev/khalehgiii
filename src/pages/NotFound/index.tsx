4cdf3ad34f19a0740ad9d9d0faa2bd97614fe88a
mahdi jafarabadi <m.jafarabadi@npd-co.ir>
2024-08-19 07:02:24 -0400

    feat  :  fix  link notfound

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -3,14 +3,16 @@ import { BaseButton } from '@ui/atoms/BaseButton';
 import { NoResult } from '@ui/molecules/NoResult';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import { useUserContext } from '@context/user/userContext';
 
 function NotFoundPage() {
   const { t } = useTranslation();
+  const { user } = useUserContext();
 
   return (
     <div className="p-16 h-screen flex flex-col justify-center items-center font-on">
       <NoResult description={t('global.notFoundPage')} />
-      <Link to={ROUTES_PATH.dashboard}>
+      <Link to={user === null ? ROUTES_PATH.home : ROUTES_PATH.dashboard}>
         <BaseButton
           className="mt-6"
           label={t('global.goToHome')}
