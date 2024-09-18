7199bd9135c84053d6260fc4726b2717bdc2b120
mahdi jafarabadi <m.jafarabadi@npd-co.ir>
2024-09-18 02:25:16 -0400

    fix : base button z-index

--- src/components/atoms/BaseButton/styles.ts
+++ src/components/atoms/BaseButton/styles.ts
@@ -5,7 +5,7 @@ const disabledClass =
   'disabled:opacity-100 disabled:bg-neutral-100 disabled:text-neutral-400 disabled:border-neutral-400 disabled:border-2 disabled:shadow-none ';
 
 export const baseButtonStyles = cva(
-  `flex items-center justify-center transition duration-150 ease-in-out rounded-md focus:outline-none p-px z-10  dark:bg-black ${SIZE.TYPOGRAPHY.BODY2} ${disabledClass}`,
+  `flex items-center justify-center transition duration-150 ease-in-out rounded-md focus:outline-none p-px  dark:bg-black ${SIZE.TYPOGRAPHY.BODY2} ${disabledClass}`,
   {
     variants: {
       type: {
