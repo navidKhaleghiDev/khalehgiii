65244938d72c438755a960b996369dce5e932864
m.talebi <mahita.contact@gmail.com>
2024-10-09 10:31:12 -0400

    feat: revert otp form

--- src/pages/Login/LoginSteps/index.tsx
+++ src/pages/Login/LoginSteps/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
@@ -8,7 +8,12 @@ import userIcon from '@iconify-icons/ph/user';
 import languageIcon from '@iconify-icons/ph/globe-thin';
 import { Avatar, BaseButton } from '@redesignUi/atoms';
 import { DropDownWithIcon } from '@ui/atoms/DropDownWithIcon';
-import { API_USERS_LOGIN, API_USERS_PROFILE } from '@src/services/users';
+import {
+  API_USERS_LOGIN,
+  API_USERS_LOGIN_OTP,
+  API_USERS_PROFILE,
+} from '@src/services/users';
+
 import { languageOptions } from '@src/constants/optios';
 import { ROUTES_PATH } from '@src/routes/routesConstants';
 import { STORAGE_KEY_REFRESH_TOKEN, http } from '@src/services/http';
@@ -17,6 +22,7 @@ import { useUserContext } from '@context/user/userContext';
 
 import { LoginFieldValues } from '../types';
 import { LoginForm } from '../LoginForm';
+import LogInOtpForm from '../LoginOtpForm';
 
 export function LoginSteps() {
   const { setUser } = useUserContext();
@@ -25,6 +31,7 @@ export function LoginSteps() {
   const { t } = useTranslation();
 
   const [error, setError] = useState<string | null>(null);
+  const [isOtpActive, setIsOtpActive] = useState<boolean>(false);
   const [loadingButton, setLoadingButton] = useState(false);
 
   const { control, handleSubmit } = useForm<LoginFieldValues>({
@@ -59,6 +66,7 @@ export function LoginSteps() {
 
         toast.success(t('global.successfullyLogedIn'));
         navigate(ROUTES_PATH.dashboard);
+        setIsOtpActive(true);
       })
       .catch((err) => {
         setUser(null);
@@ -71,24 +79,35 @@ export function LoginSteps() {
   }, [navigate, setUser, t]);
 
   const handleSubmitForm = useCallback(
-    async ({ email, password }: LoginFieldValues) => {
+    async ({ email, password, totp }: LoginFieldValues) => {
       setLoadingButton(true);
+
       try {
-        const response = await API_USERS_LOGIN({ email, password });
+        const response = isOtpActive
+          ? await API_USERS_LOGIN_OTP({ email, password, totp })
+          : await API_USERS_LOGIN({ email, password });
 
         const { data } = response;
-
-        localStorage.setItem(STORAGE_KEY_REFRESH_TOKEN, data.refresh_token);
-        http.setAuthHeader(data.access_token, data.refresh_token);
-        handleGetProfile();
+        const needOtp = data.info === 'ready to get totp to verify';
+        if (needOtp) {
+          setIsOtpActive(true);
+        } else {
+          localStorage.setItem(STORAGE_KEY_REFRESH_TOKEN, data.refresh_token);
+          http.setAuthHeader(data.access_token, data.refresh_token);
+          handleGetProfile();
+        }
       } catch (err) {
         setError(err as string);
       } finally {
         setLoadingButton(false);
       }
     },
-    [handleGetProfile]
+    [handleGetProfile, isOtpActive]
   );
+
+  useEffect(() => {
+    setError(null);
+  }, [isOtpActive]);
 
   return (
     <div className="flex flex-col items-center w-full mt-auto">
@@ -107,8 +126,15 @@ export function LoginSteps() {
         <div>
           <Avatar icon={userIcon} size="lg" className="mb-[0.81rem]" />
         </div>
-
-        <LoginForm control={control} error={error} />
+        {isOtpActive ? (
+          <LogInOtpForm
+            control={control}
+            setIsOtpActive={setIsOtpActive}
+            error={error}
+          />
+        ) : (
+          <LoginForm control={control} error={error} />
+        )}
 
         <BaseButton
           label={t('login.login')}
