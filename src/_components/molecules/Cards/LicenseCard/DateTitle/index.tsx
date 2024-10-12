84e0119afc59e2a16ed18042a905fccaa2d90c34
m.talebi <mahita.contact@gmail.com>
2024-10-12 08:21:26 -0400

    feat: change locate & add useTranslate

---
+++ src/_components/molecules/Cards/LicenseCard/DateTitle/index.tsx
@@ -0,0 +1,35 @@
+import { useTranslation } from 'react-i18next';
+import { VariantProps } from 'class-variance-authority';
+
+import { Typography } from '@redesignUi/atoms';
+
+import { dateTitleStyle } from './styles';
+
+export interface DateTitleProps extends VariantProps<typeof dateTitleStyle> {
+  date: string;
+}
+
+export function DateTitle({ date, color }: DateTitleProps) {
+  const { t } = useTranslation();
+  return (
+    <div
+      className={`flex items-center gap-[0.18rem] ${dateTitleStyle({
+        color,
+      })} 
+`}
+    >
+      <div className="flex-col gap-1 flex leading-4">
+        <Typography
+          variant="body6B"
+          color="neutralDark"
+          className="ltr:text-left rtl:text-right"
+        >
+          {t('license.expirationDate')}
+        </Typography>
+        <Typography variant="body6" color="neutralMiddle">
+          {date}
+        </Typography>
+      </div>
+    </div>
+  );
+}
