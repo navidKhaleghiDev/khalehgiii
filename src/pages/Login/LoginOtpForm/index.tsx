cc4c4dcad56f760f64383307387ea5fdc9d3fdd1
m.talebi <mahita.contact@gmail.com>
2024-10-13 07:35:49 -0400

    style: redesign login form otp

--- src/pages/Login/LoginOtpForm/index.tsx
+++ src/pages/Login/LoginOtpForm/index.tsx
@@ -1,33 +1,62 @@
+import { Dispatch, SetStateAction } from 'react';
+
 import { useTranslation } from 'react-i18next';
+import { Control, FieldValues } from 'react-hook-form';
 
 import { BackButton } from '@redesignUi/atoms/BackButton';
 import { Typography } from '@redesignUi/atoms';
 import { BaseOtp } from '@redesignUi/atoms/Inputs/BaseOtp';
 
+import { LoginFieldValues } from '../types';
+
+interface LoginOtpProps<T extends FieldValues> {
+  control: Control<T>;
+  error: string | null;
+  setIsOtpActive: Dispatch<SetStateAction<boolean>>;
+}
+
 export default function LogInOtpForm({
   control,
   setIsOtpActive,
   error,
-  name = 'totp',
-}: any) {
+}: LoginOtpProps<LoginFieldValues>) {
   const { t } = useTranslation();
 
   return (
     <div>
-      <div className="absolute top-[1.87rem] rtl:right-20 ltr:left-20">
+      <div className="absolute top-[1.87rem] rtl:right-[4.37rem] ltr:left-[4.37rem]">
         <BackButton onClick={() => setIsOtpActive(false)} />
       </div>
 
-      <Typography color="neutral" variant="body4" className="mb-8 text-center ">
-        {t('login.otp')}
-      </Typography>
+      <div className="flex flex-col gap-1 p-5 mb-[5.12rem]">
+        <Typography
+          color="black"
+          variant="body1B"
+          className="text-center leading-8"
+        >
+          {t('login.otpCode')}
+        </Typography>
+        <Typography
+          color="neutral"
+          variant="body3"
+          className="text-center leading-7"
+        >
+          {t('login.otp')}
+        </Typography>
+      </div>
 
       {error && (
         <Typography color="red" variant="body3" className="mb-2 text-center ">
           {error}
         </Typography>
       )}
-      <BaseOtp name={name} valueLength={6} control={control} size="md" />
+      <BaseOtp
+        name="totp"
+        valueLength={6}
+        control={control}
+        size="md"
+        className="mb-[12.8rem]"
+      />
     </div>
   );
 }
