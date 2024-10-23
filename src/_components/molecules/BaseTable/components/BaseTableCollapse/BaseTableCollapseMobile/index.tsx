513c66b8ebcbe70f4dad55da4f28eb930ca035e5
mahdi jafarabadi <m.jafarabadi@npd-co.ir>
2024-10-23 17:32:59 +0330

    fix :  index baseTable

--- src/_components/molecules/BaseTable/components/BaseTableCollapse/BaseTableCollapseMobile/index.tsx
+++ src/_components/molecules/BaseTable/components/BaseTableCollapse/BaseTableCollapseMobile/index.tsx
@@ -30,7 +30,7 @@ export function BaseTableCollapseMobile<T extends IdItem>(
 
   return (
     <tr className={baseTableCollapseMobile()}>
-      {header.map((headerList, index) => (
+      {header.map((headerList) => (
         <th
           key={headerList.label}
           className={`w-[90%] m-auto flex justify-between items-center border first:border-t-0 last:border-b-0  border-gray-300 border-x-0 h-10  `}
