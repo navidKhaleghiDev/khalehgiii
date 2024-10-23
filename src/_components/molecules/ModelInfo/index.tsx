645345b02a86b15ca9cf7b62380b3869e5dc4b24
navidXlg <navidjoker669@gmail.com>
2024-10-23 11:24:13 +0330

    feat: add infoModal darkMode

--- src/_components/molecules/ModelInfo/index.tsx
+++ src/_components/molecules/ModelInfo/index.tsx
@@ -1,29 +1,54 @@
 import { useRef } from 'react';
 
 import { useClickOutside } from '@src/helper/hooks/useClickOutside';
-import { BaseIcon } from '@redesignUi/atoms';
-import PhMagnifyingGlass from '@iconify-icons/ph/magnifying-glass';
+import { IconButton } from '@redesignUi/atoms/BaseButton';
+import { BaseIcon, Typography } from '@redesignUi/atoms';
+import X from '@iconify-icons/ph/x';
 
-import { ModalProps } from '../Modal/types';
+import { ModelInfoProps } from '../Modal/types';
 
-export function ModalInfo(props: ModalProps): JSX.Element | null {
-  const { open, setOpen, classContainer } = props;
+export function ModalInfo(props: ModelInfoProps): JSX.Element | null {
+  const { open, setOpen, classContainer, icon, title, description, content } =
+    props;
   const ref = useRef(null);
   useClickOutside({ ref, setValue: setOpen, value: open });
-  // const handleToggle = () => setOpen(!open);
+  const handleToggle = () => setOpen(!open);
 
   return open ? (
     <div className="fixed w-full h-100 inset-0 z-50 animated fadeIn overflow-hidden flex justify-center items-center backdrop-blur-sm">
       <div
         ref={ref}
-        className={`rounded-[1.25rem] shadow-lg modal-container bg-white dark:bg-neutral-600 mx-auto z-50 overflow-y-auto ${classContainer}`}
+        className={`rounded-[1.25rem] w-[334px] md:w-[634px] shadow-lg modal-container bg-white dark:bg-gray-700 mx-auto z-50 overflow-y-auto ${classContainer}`}
       >
-        <div className="text-center p-5">
-          <div>
-            <div className="border p-3 border-gray-200 rounded-lg shadow-sm">
-              <BaseIcon icon={PhMagnifyingGlass} size="md" color="neutral" />
+        <div className="p-5">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-5">
+              <div className="border p-3 border-gray-200 dark:border-gray-500 rounded-lg shadow-sm">
+                <BaseIcon
+                  icon={icon}
+                  size="md"
+                  className="text-gray-500 dark:text-gray-400"
+                />
+              </div>
+              <div className="flex flex-col text-start">
+                <Typography color="neutralDark" variant="body3B">
+                  {title}
+                </Typography>
+                {description && (
+                  <Typography color="neutralMiddle" variant="body5">
+                    {description}
+                  </Typography>
+                )}
+              </div>
             </div>
+            <IconButton
+              icon={X}
+              color="neutralNoBg"
+              onClick={handleToggle}
+              size="md"
+            />
           </div>
+          {content && <div>{content}</div>}
         </div>
       </div>
     </div>
