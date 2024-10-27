b0c2df43d35fd5bbae4a68e9d30cfed7a11821a3
ferdos.a <ferdos.a@npdco.local>
2024-10-27 12:52:06 +0330

    fix(DashboardAdminLogs): replace mock data with Table component
    - Integrated Table component and removed mock data
    - Added real data to display in the Table component
    - Updated translation files and added new keys as needed
    - SEP-75: Redesign Dashboard

--- src/pages/Dashboard/DashboardAdminLogs/index.tsx
+++ src/pages/Dashboard/DashboardAdminLogs/index.tsx
@@ -1,9 +1,6 @@
-import moment from 'moment-jalaali';
 import { useState } from 'react';
 import useSWR from 'swr';
 
-import { Card, Typography } from '@redesignUi/atoms';
-import { LoadingPage } from '@redesignUi/molecules/Loading';
 import { checkPermission } from '@src/helper/hooks/usePermission';
 // import { useUserPermission } from '@src/helper/hooks/usePermission';
 import { createAPIEndpoint } from '@src/helper/utils';
@@ -13,10 +10,12 @@ import { E_USERS } from '@src/services/users/endpoint';
 import { IUser } from '@src/services/users/types';
 import { EPermissionUsers, PermissionsCodeName } from '@src/types/permissions';
 import { IResponsePagination } from '@src/types/services';
+import { BaseMiniTable } from '@redesignUi/molecules/BaseMiniTable';
+import { t } from 'i18next';
 // import { BaseTable } from '@ui/atoms/BaseTable';
 // import { checkPermissionHeaderItem } from '@ui/atoms/BaseTable/components/utils/CheckPermissionHeaderItem';
 
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 4;
 const PAGE = 1;
 
 export function DashboardAdminLogs({
@@ -24,66 +23,50 @@ export function DashboardAdminLogs({
 }: {
   permissions: PermissionsCodeName[];
 }) {
-  const [currentPage] = useState<number>(PAGE);
-  const [filterQuery] = useState<string>('');
+  const [currentPage, setCurrentPage] = useState<number>(PAGE);
 
   const endpoint = createAPIEndpoint({
     endPoint: E_USERS,
     pageSize: PAGE_SIZE,
     currentPage,
-    filterQuery,
+    filterQuery: '',
   });
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
 
   const { data, isLoading } = useSWR<IResponsePagination<IUser>>(
     endpoint,
     http.fetcherSWR
   );
+  const countPage = data?.data?.count || 0;
+
+  const paginationProps = {
+    countPage,
+    currentPage,
+    totalPages: Math.ceil(countPage / PAGE_SIZE),
+    onPageChange: handlePageChange,
+  };
   const listWhiteList = data?.data?.results ?? [];
 
-  if (isLoading) {
-    return <LoadingPage />;
-  }
+  const header = {
+    email: t('table.userName'),
+    last_login: t('table.lastLogin'),
+    last_logout: t('table.lastLogin'),
+  };
 
   return (
     checkPermission(permissions, EPermissionUsers.VIEW) && (
-      <Card
-        color="white"
-        rounded="xxl"
-        shadow="base"
-        className="xl:p-5 py-6 px-5 xl:h-[17.5rem] h-[13rem] overflow-x-hidden overflow-y-auto"
-      >
-        <Typography color="black" variant="body4B" className="mb-2.5">
-          ورود و خروج ادمین ها
-        </Typography>
-        <table className="table-auto w-full border-separate border-spacing-x-0 border-spacing-y-2.5">
-          <thead>
-            <tr className="text-xs font-normal">
-              <th className="text-xs font-normal px-2 py-1">نام ادمین</th>
-              <th className="text-xs font-normal px-2 py-1">آخرین ورود</th>
-              <th className="text-xs font-normal px-2 py-1">آخرین خروج</th>
-            </tr>
-          </thead>
-          <tbody>
-            {listWhiteList.map((user) => (
-              <tr className="border border-gray-300 rounded-sm" key={user.id}>
-                <td className="text-xs font-normal px-2 py-1 text-center">
-                  {user.email}
-                </td>
-                <td className="text-xs font-normal px-2 py-1 text-center">
-                  {user.last_login
-                    ? moment(user.last_login).format('jYYYY/jMM/jDD')
-                    : '---'}
-                </td>
-                <td className="text-xs font-normal px-2 py-1 text-center">
-                  {user.last_login
-                    ? moment(user.last_login).format('jYYYY/jMM/jDD')
-                    : '---'}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </Card>
+      <BaseMiniTable
+        header={header}
+        body={listWhiteList}
+        loading={isLoading}
+        date={['last_login', 'last_logout']}
+        pagination={paginationProps}
+        title={t('dashboard.adminLogin')}
+        className="w-full xl:[&>*:last-child>*:last-child>*]:max-h-10 md:[&>*:last-child>*:last-child>*]:max-h-6 [&>*:last-child>*:last-child>*]:max-h-10 xl:h-[17.5rem] md:h-[13.125rem] h-auto"
+      />
     )
   );
 }
