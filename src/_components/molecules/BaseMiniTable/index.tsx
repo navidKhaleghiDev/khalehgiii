ea9b260df230cf1361adea8bb3d5f2d939beef56
mahdi jafarabadi <m.jafarabadi@npd-co.ir>
2024-10-27 11:49:31 +0330

    update : baseMiniTable date

--- src/_components/molecules/BaseMiniTable/index.tsx
+++ src/_components/molecules/BaseMiniTable/index.tsx
@@ -5,9 +5,10 @@ import { BaseMiniTableSkeleton } from './loading';
 import { BaseMiniTableNoResult } from './components/BaseMiniTableNoResult';
 import { BaseMiniTableProps, IdItem } from './types';
 import { Pagination } from '../Pagination';
+import { BaseTableDateCell } from '../BaseTable/components/BaseTableRowCells/BaseTableDateCell';
 
 export function BaseMiniTable<T extends IdItem>(props: BaseMiniTableProps<T>) {
-  const { header, body, loading, pagination, title } = props;
+  const { header, body, loading, pagination, title, date, className } = props;
   const { currentPage, totalPages, onPageChange } = pagination;
 
   const { isFarsi } = useLanguage();
@@ -15,14 +16,18 @@ export function BaseMiniTable<T extends IdItem>(props: BaseMiniTableProps<T>) {
     ? 'group-hover:-translate-x-[50%]'
     : 'group-hover:translate-x-[50%]';
 
+  const isDateKey = (key: string) => {
+    return Array.isArray(date) ? date.includes(key) : key === date;
+  };
+
   return (
     <Card
-      className="w-[445px] h-[280px] flex-col justify-center items-center  p-5"
+      className={`w-[445px] h-[280px] flex-col justify-center items-center p-5 ${className}`}
       color="white"
       shadow="base"
       rounded="xl"
     >
-      <div className="flex justify-between mb-1 ">
+      <div className="flex justify-between mb-1">
         <Typography variant="body5B" color="black">
           {title}
         </Typography>
@@ -35,10 +40,10 @@ export function BaseMiniTable<T extends IdItem>(props: BaseMiniTableProps<T>) {
       </div>
 
       <div className="w-full">
-        <div className="w-full text-xs bg-white dark:bg-gray-500 text-gray-500  rounded-lg  dark:text-gray-200 ">
+        <div className="w-full text-xs bg-white dark:bg-gray-500 text-gray-500 rounded-lg dark:text-gray-200">
           <div className="flex">
             {Object.keys(header).map((key) => (
-              <div key={key} className="flex-1 px-4 py-1 ">
+              <div key={key} className="flex-1 px-4 py-1">
                 {header[key]}
               </div>
             ))}
@@ -53,18 +58,22 @@ export function BaseMiniTable<T extends IdItem>(props: BaseMiniTableProps<T>) {
               body.map((item) => (
                 <div
                   key={item.id}
-                  className=" max-h-7 mt-1 bg-white dark:bg-gray-600 rounded-lg border border-gray-100 "
+                  className="max-h-7 mt-1 bg-white dark:bg-gray-600 rounded-lg border border-gray-100"
                 >
                   <div className="flex">
                     {Object.keys(header).map((key) => (
                       <div
                         key={key}
-                        className="group flex-1 px-4 py-2 text-gray-900 dark:text-gray-300 text-xs overflow-hidden "
+                        className="group flex-1 px-4 py-2 text-gray-900 dark:text-gray-300 text-xs overflow-hidden"
                       >
                         <Typography
-                          className={`!text-ellipsis whitespace-nowrap group-hover:whitespace-nowrap group-hover:overflow-visible transition-transform duration-1000 ease-linear ${directionStyle} group-hover:duration-[1000ms] `}
+                          className={`!text-ellipsis whitespace-nowrap group-hover:whitespace-nowrap group-hover:overflow-visible transition-transform duration-1000 ease-linear ${directionStyle} group-hover:duration-[1000ms]`}
                         >
-                          {item[key]}
+                          {isDateKey(key) ? (
+                            <BaseTableDateCell id={key} row={item} />
+                          ) : (
+                            item[key]
+                          )}
                         </Typography>
                       </div>
                     ))}
