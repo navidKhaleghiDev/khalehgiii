24da38d5fa0a1435c07f18dd17ccd08b1367c119
m.talebi <mahita.contact@gmail.com>
2024-11-02 08:14:22 -0400

    fix: add new feature file in table

---
+++ src/_components/molecules/BaseTable/components/utils/CheckPermissionHeaderItem/index.ts
@@ -0,0 +1,42 @@
+import { HeaderTable } from '@redesignUi/molecules/BaseTable/types';
+import { PermissionsCodeName } from '@src/types/permissions';
+
+export function checkPermissionHeaderItem(
+  userPermissions: PermissionsCodeName[],
+  headerItems: HeaderTable[]
+): HeaderTable[] {
+  // Filter header items based on user permissions
+  const filteredHeaderItems = headerItems.filter((item: HeaderTable) => {
+    // Check if the item itself requires a permission
+    if (Array.isArray(item.permission)) {
+      if (
+        !item.permission.some((permission) =>
+          userPermissions.includes(permission)
+        )
+      ) {
+        return false;
+      }
+    } else if (item.permission && !userPermissions.includes(item.permission)) {
+      return false;
+    }
+
+    // Check if the item has actions that require permissions
+    if (item.action) {
+      // eslint-disable-next-line no-param-reassign
+      item.action = item.action.filter(
+        (actionItem: any) =>
+          !actionItem.permission ||
+          userPermissions.includes(actionItem.permission)
+      );
+
+      // If no actions are left after filtering, exclude this item
+      if (item.action.length === 0) {
+        return false;
+      }
+    }
+
+    return true;
+  });
+
+  return filteredHeaderItems;
+}
