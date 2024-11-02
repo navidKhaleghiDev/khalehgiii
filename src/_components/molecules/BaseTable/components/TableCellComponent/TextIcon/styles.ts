fbbbf24ef55076af55c4667beaf313acd3dda8b4
ferdos.a <ferdos.a@npdco.local>
2024-11-02 15:44:27 +0330

    feat: add dark color at cell components table

--- src/_components/molecules/BaseTable/components/TableCellComponent/TextIcon/styles.ts
+++ src/_components/molecules/BaseTable/components/TableCellComponent/TextIcon/styles.ts
@@ -1,13 +1,12 @@
 import { cva } from 'class-variance-authority';
 
 export const TextIconStyle = cva(
-  'flex justify-center items-center rounded gap-1 px-2 py-0.5',
+  'flex justify-center items-center rounded gap-1 px-2 py-0.5 md:dark:bg-transparent dark:bg-gray-600',
   {
     variants: {
       color: {
-        blue: 'bg-blue-100 text-blue-500 dark:bg-transparent dark:text-blue-300',
-        purple:
-          'bg-purple-100 text-purple-500 dark:bg-transparent dark:text-purple-400',
+        blue: 'bg-blue-100 text-blue-500 dark:text-blue-300',
+        purple: 'bg-purple-100 text-purple-500 dark:text-purple-400',
       },
     },
   }
