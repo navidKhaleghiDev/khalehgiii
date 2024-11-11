bf59bf00a42fb62d5bab6acae1a300cbb1789b82
m.talebi <mahita.contact@gmail.com>
2024-11-11 03:12:45 -0500

    style: ok style

--- src/_components/molecules/BaseTable/styles.ts
+++ src/_components/molecules/BaseTable/styles.ts
@@ -15,9 +15,9 @@ export const baseTableRowStyles = cva(
   {
     variants: {
       isOpen: {
-        true: 'bg-gray-100 border border-gray-400 dark:bg-gray-800',
+        true: 'bg-gray-100 border border-gray-400 dark:border-gray-500 dark:bg-gray-800',
         false:
-          'bg-white border border-gray-200 dark:bg-gray-600 dark:border-gray-400',
+          'bg-white border border-gray-200 dark:bg-gray-600 dark:border-gray-500',
       },
       radiusTop: {
         true: ' rounded-t-lg md:rounded-t-2xl',
@@ -31,10 +31,10 @@ export const baseTableRowStyles = cva(
   }
 );
 export const baseTableCollapseDesktop = cva(
-  `py-2 gap-4 flex justify-start h-full bg-gray-100 border border-gray-400 border-t-0 transition duration-150 ease-in-out  dark:bg-gray-800`
+  `py-2 gap-4 flex justify-start h-full bg-gray-100 border border-gray-400 dark:border-gray-500 border-t-0 transition duration-150 ease-in-out  dark:bg-gray-800`
 );
 export const baseTableCollapseMobile = cva(
-  `bg-gray-100 border border-gray-300  dark:bg-gray-800  `
+  `bg-gray-100 border border-gray-400 dark:border-gray-500 dark:bg-gray-800  `
 );
 export const baseTableMenuCell = cva(
   `flex gap-3 !justify-start border-none h-6 w-full font-light leading-none `,
