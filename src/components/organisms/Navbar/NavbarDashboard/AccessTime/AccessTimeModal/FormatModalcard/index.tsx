532eb8ae6d162a3d13f7fb51b8f3d55c8f6d94ae
navidXlg <navidjoker669@gmail.com>
2024-11-12 17:20:28 +0330

    Fix: remove the groupeFilter in filterList

--- src/components/organisms/Navbar/NavbarDashboard/AccessTime/AccessTimeModal/FormatModalcard/index.tsx
+++ src/components/organisms/Navbar/NavbarDashboard/AccessTime/AccessTimeModal/FormatModalcard/index.tsx
@@ -5,6 +5,14 @@ type FormatModalCardProps = {
   formatList?: object;
 };
 
+function convertMbToGb(mbValue: number): string {
+  if (mbValue >= 1000) {
+    const gbValue = mbValue / 1024;
+    return `${gbValue.toFixed(2)} GB`;
+  }
+  return `${mbValue} MB`;
+}
+
 export function FormatModalCard({ label, formatList }: FormatModalCardProps) {
   return (
     <div className="text-start w-[255px]">
@@ -15,12 +23,14 @@ export function FormatModalCard({ label, formatList }: FormatModalCardProps) {
         {formatList
           ? Object.entries(formatList).map(([key, value]) => (
               <div
-                className="bg-neutral-100 px-2.5 py-1 rounded-md text-neutral-800"
+                className="bg-neutral-100 px-2.5 py-1 h-fit rounded-md text-neutral-800"
                 key={`${key} ${value}`}
                 dir="ltr"
               >
                 <div className="flex flex-row-reverse text-sm items-center justify-center gap-[4px]">
-                  <p className="text-sm text-gray-400">{value as string}MB</p>
+                  <p className="text-sm text-gray-400">
+                    {convertMbToGb(value)}
+                  </p>
                   <p>{key}</p>
                 </div>
               </div>
