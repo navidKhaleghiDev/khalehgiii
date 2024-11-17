3f7e1723f8c62869d1e03fa12f5060af70f61b8e
navidXlg <navidjoker669@gmail.com>
2024-11-17 14:16:04 +0330

    fix: comment the items that are not in the design

--- src/pages/FIleReports/ReportFileScanPage/ScanFIleList/index.tsx
+++ src/pages/FIleReports/ReportFileScanPage/ScanFIleList/index.tsx
@@ -1,4 +1,3 @@
-import DateObject from 'react-date-object';
 import { toast } from 'react-toastify';
 import { useTranslation } from 'react-i18next';
 import { useState } from 'react';
@@ -9,61 +8,39 @@ import { IResponsePagination } from '@src/types/services';
 import { IScannedFile } from '@src/services/analyze/types';
 import { E_ANALYZE_SCAN_PAGINATION } from '@src/services/analyze/endpoint';
 import { OnClickActionsType } from '@redesignUi/molecules/BaseTable/types';
-import { MultiDatePicker } from '@redesignUi/atoms/Inputs/DatePicker';
-import {
-  API_ANALYZE_DOWNLOAD_FILE,
-  API_ANALYZE_SCAN_STATUS_UPDATE,
-} from '@src/services/analyze';
+import { API_ANALYZE_DOWNLOAD_FILE } from '@src/services/analyze';
 import { checkPermissionHeaderItem } from '@redesignUi/molecules/BaseTable/components/utils/CheckPermissionHeaderItem';
-import { Modal } from '@redesignUi/molecules/Modal';
 import { BaseTable } from '@redesignUi/molecules/BaseTable';
 import { useUserPermission } from '@src/helper/hooks/usePermission';
 import { useWindowDimensions } from '@src/helper/hooks/useWindowDimensions';
-import { MultiDatePickerProps } from '@redesignUi/atoms/Inputs/DatePicker/types';
 
 import {
   scannedFileHeaderWithOutPermission,
   scannedFileHeaderWithPermission,
 } from './constants/scannedFileHeaderItem';
+import { ScanFileDatePicker } from './ScanFIleDatePicker';
 
 const PAGE_SIZE = 8;
 const PAGE = 1;
 
-type ScannedFileListProp = {
-  userEmail: string;
-};
-
-export function ScannedFileList({ userEmail }: ScannedFileListProp) {
-  const [activeScannedFile, setActiveScannedFile] = useState<IScannedFile>();
-  const [cleanStatusModal, setCleanStatusModal] = useState(false);
-  const [dateRange, setRangeDate] = useState<DateObject[] | undefined>();
+export function ScannedFileList({ userEmail }: { userEmail: string }) {
   const [currentPage, setCurrentPage] = useState<number>(PAGE);
-  const [loading, setLoading] = useState<boolean>(false);
 
   const userPermissions = useUserPermission();
   const { width } = useWindowDimensions();
   const { t } = useTranslation();
 
-  const handleDatePickerFilter: MultiDatePickerProps['onChange'] = (date) => {
-    setRangeDate(date);
-    console.log(dateRange);
-  };
-
-  // DaAs users scan reports
-  const { data, isLoading, mutate, error } = useSWR<
-    IResponsePagination<IScannedFile>
-  >(
-    userEmail
-      ? E_ANALYZE_SCAN_PAGINATION(userEmail, {
-          page: currentPage,
-          pageSize: PAGE_SIZE,
-        })
-      : null,
+  // Daas user scan reports
+  const { data, isLoading, error } = useSWR<IResponsePagination<IScannedFile>>(
+    E_ANALYZE_SCAN_PAGINATION(userEmail, {
+      page: currentPage,
+      pageSize: PAGE_SIZE,
+    }),
     HTTP_ANALYSES.fetcherSWR
   );
-
   const scanFilesList = data?.data?.results ?? [];
   const countPage = data?.data?.count ?? 0;
+  // Daas user download file permission
   const evidencePermissions =
     scanFilesList[scanFilesList.length - 1]?.evidence_permission;
 
@@ -91,31 +68,34 @@ export function ScannedFileList({ userEmail }: ScannedFileListProp) {
       });
   };
 
-  const cleanStatus = async () => {
-    if (activeScannedFile) {
-      setLoading(true);
-      await API_ANALYZE_SCAN_STATUS_UPDATE(activeScannedFile)
-        .then(() => {
-          mutate();
-        })
-        .catch((err) => {
-          toast.error(err);
-        })
-        .finally(() => setLoading(false));
-    }
+  // There is no item for updating the scanFile
+  // const cleanStatus = async () => {
+  //   if (activeScannedFile) {
+  //     setLoading(true);
+  //     await API_ANALYZE_SCAN_STATUS_UPDATE(activeScannedFile)
+  //       .then(() => {
+  //         mutate();
+  //       })
+  //       .catch((err) => {
+  //         toast.error(err);
+  //       })
+  //       .finally(() => setLoading(false));
+  //   }
+  // };
+
+  const handelClickRow: OnClickActionsType<IScannedFile> = (action, item) => {
+    if (action === 'download') downloadFile(item);
+
+    // There is no item for editing the items
+    // } else if (action === 'edit') {
+    //   setActiveScannedFile({ ...item, scan_result: 'CLEAN' } as IScannedFile);
+    //   setCleanStatusModal(true);
+    // } else {
+    //   setActiveScannedFile(item as IScannedFile);
+    // }
   };
 
-  const handleOpenModal: OnClickActionsType<IScannedFile> = (action, item) => {
-    if (action === 'download') {
-      downloadFile(item);
-    } else if (action === 'edit') {
-      setActiveScannedFile({ ...item, scan_result: 'CLEAN' } as IScannedFile);
-      setCleanStatusModal(true);
-    } else {
-      setActiveScannedFile(item as IScannedFile);
-    }
-  };
-
+  // Table data
   const paginationProps = {
     countPage,
     currentPage,
@@ -125,20 +105,13 @@ export function ScannedFileList({ userEmail }: ScannedFileListProp) {
     allItems: countPage,
     itemsPer: scanFilesList.length,
   };
-
   const scanFileHeader = evidencePermissions
     ? scannedFileHeaderWithPermission
     : scannedFileHeaderWithOutPermission;
 
   return (
     <div className="w-full">
-      <div className="text-start my-5">
-        <MultiDatePicker
-          id="recordFilter"
-          name="recordFilter"
-          onChange={(date) => handleDatePickerFilter(date)}
-        />
-      </div>
+      <ScanFileDatePicker />
       <div className="[&_thead]:bg-gray-100">
         {!error ? (
           <BaseTable
@@ -146,7 +119,7 @@ export function ScannedFileList({ userEmail }: ScannedFileListProp) {
             header={checkPermissionHeaderItem(userPermissions, scanFileHeader)}
             loading={isLoading}
             pagination={paginationProps}
-            onClick={handleOpenModal}
+            onClick={handelClickRow}
             isMobile={width <= 760}
           />
         ) : (
@@ -154,7 +127,8 @@ export function ScannedFileList({ userEmail }: ScannedFileListProp) {
           <p className="flex items-center justify-center">{error}</p>
         )}
       </div>
-      <Modal
+      {/* This modal is for the old design */}
+      {/* <Modal
         open={cleanStatusModal}
         setOpen={setCleanStatusModal}
         type="error"
@@ -169,7 +143,7 @@ export function ScannedFileList({ userEmail }: ScannedFileListProp) {
           onClick: () => setCleanStatusModal(false),
           color: 'red',
         }}
-      />
+      /> */}
     </div>
   );
 }
