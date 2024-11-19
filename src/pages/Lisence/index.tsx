a9ae9c0c90a33ede8c01d8641cb5d00a374719cb
shabani.f <shabanifateme22@gmail.com>
2024-11-19 10:55:31 +0330

    delete permision for lisence

--- src/pages/Lisence/index.tsx
+++ src/pages/Lisence/index.tsx
@@ -8,9 +8,7 @@ import { BaseTable } from '@redesignUi/molecules/BaseTable';
 import { useWindowDimensions } from '@src/helper/hooks/useWindowDimensions';
 import { createAPIEndpoint } from '@src/helper/utils';
 import { IResponsePagination } from '@src/types/services';
-import { useUserPermission } from '@src/helper/hooks/usePermission';
 import { Typography } from '@redesignUi/atoms';
-import { checkPermissionHeaderItem } from '@redesignUi/molecules/BaseTable/components/utils/CheckPermissionHeaderItem';
 
 import { LicenseFileType } from './type';
 import { LisenceHeaderItem } from './constant/LisenceHeaderItem';
@@ -21,7 +19,6 @@ export function License() {
   const [currentPage, setCurrentPage] = useState<number>(PAGE);
   const { t } = useTranslation();
   const dimensions = useWindowDimensions();
-  const userPermissions = useUserPermission();
 
   const endpoint = createAPIEndpoint({
     endPoint: E_USERS_LICENSES,
@@ -33,6 +30,7 @@ export function License() {
     endpoint,
     http.fetcherSWR
   );
+  
   const handlePageChange = (newPage: number) => {
     setCurrentPage(newPage);
   };
@@ -63,7 +61,7 @@ export function License() {
 
       <BaseTable
         body={listLicense as LicenseFileType[]}
-        header={checkPermissionHeaderItem(userPermissions, LisenceHeaderItem)}
+        header={LisenceHeaderItem}
         loading={isLoading}
         pagination={paginationProps}
         isMobile={dimensions.width <= 770}
